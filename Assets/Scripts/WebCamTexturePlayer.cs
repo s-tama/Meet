1954cd0d83d110a9193cdd5362a282574b0f1f82
s-tamamura <s-tamamura@liberent.co.jp>
2020-09-20 01:01:16 +0900

    更新

--- Assets/Scripts/WebCamTexturePlayer.cs
+++ Assets/Scripts/WebCamTexturePlayer.cs
@@ -1,38 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
+
+using Process = System.Diagnostics.Process;
 
 namespace MyProject
 {
     [RequireComponent(typeof(RawImage))]
     public class WebCamTexturePlayer : MonoBehaviour
     {
+        // プロセスを起動させるbatファイルが格納されたディレクトリのパス
+        static readonly string ProcessScriptPath = "/StreamingAssets/ProcessScripts/";
+
         [SerializeField] Shader _shader = null;
         Material _material;
 
         [SerializeField] Color _color = Color.white;
+
+        [SerializeField] string _deviceName = "Camera";
 
         Texture _webCamTexture;
         RawImage _image;
 
         void Start()
         {
-            _material = new Material(_shader);
-            _image = GetComponent<RawImage>();
-
-            Debug.Log(WebCamTexture.devices.Length);
-            for (int i = 0; i < WebCamTexture.devices.Length; i++)
+            if(_deviceName == "Snap Camera" )
             {
-                Debug.Log(WebCamTexture.devices[i].name);
+                CallBat( "Snap Camera.bat" );
+                CallBat( "obs64.bat" );
             }
 
-            WebCamTexture webCamTexture = new WebCamTexture(WebCamTexture.devices[1].name);
-            _webCamTexture = webCamTexture;
-
-            webCamTexture.Play();
+            StartWebCamera();
         }
 
         void Update()
         {
+            if( _webCamTexture == null )
+            {
+                Debug.LogAssertion( "WebCamTextureの取得に失敗しています" );
+                return;
+            }
+
             RenderTexture dstTex = RenderTexture.GetTemporary(
                 _webCamTexture.width,
                 _webCamTexture.height,
@@ -44,6 +53,55 @@ namespace MyProject
             _image.texture = dstTex;
 
             RenderTexture.ReleaseTemporary(dstTex);
+        }
+
+        /// <summary>
+        /// バッチファイルを呼び出す
+        /// </summary>
+        void CallBat( string path )
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = Application.dataPath + ProcessScriptPath + path;
+
+            // 外部プロセス終了を検知
+            process.EnableRaisingEvents = true;
+            process.Exited += ( sender, e ) =>
+            {
+                process.Dispose();
+                process = null;
+            };
+
+            process.Start();
+        }
+
+        /// <summary>
+        /// Webカメラの開始
+        /// </summary>
+        void StartWebCamera()
+        {
+            _material = new Material( _shader );
+            _image = GetComponent<RawImage>();
+
+            // デバイス一覧の表示
+            Debug.Log( "デバイス数 : " + WebCamTexture.devices.Length );
+            for( int i = 0; i < WebCamTexture.devices.Length; i++ )
+            {
+                Debug.Log( i + " : " + WebCamTexture.devices[ i ].name );
+            }
+
+            // ウェブカメラテクスチャの生成
+            for( int i = 0; i < WebCamTexture.devices.Length; i++ )
+            {
+                if( WebCamTexture.devices[ i ].name == _deviceName )
+                {
+                    WebCamTexture webCamTexture = new WebCamTexture( WebCamTexture.devices[ i ].name );
+                    _webCamTexture = webCamTexture;
+                    webCamTexture.Play();
+                    break;
+                }
+            }
+
+            Debug.Assert( _webCamTexture != null, "WebCamTextureの取得に失敗しました" );
         }
     }
 }
---
+++ Assets/StreamingAssets/ProcessScripts/Snap Camera.bat
@@ -0,0 +1,3 @@
+echo off
+cd C:\Program Files\Snap Inc\Snap Camera
+"Snap Camera.exe"
